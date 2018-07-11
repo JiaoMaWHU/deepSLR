ca838e31b444aac7b8fa12af426e89d67c5500c2
Jiaoma <jiaomajiaoma@hotmail.com>
2018-07-11 21:37:21 +0800

    add 2 files

---
+++ deep_slr_v1.py
@@ -0,0 +1,371 @@
+import argparse
+import tensorflow as tf
+import logging
+import numpy as np
+import math
+import os
+import LoadData as DATA
+from time import time
+from sklearn.metrics import mean_squared_error
+from tensorflow.contrib.layers import batch_norm as batch_norm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run DeepSLR.")
+    parser.add_argument('--process', nargs='?', default='train',
+                        help='Process type: train, evaluate.')
+    parser.add_argument('--path', nargs='?', default='../data/',
+                        help='Input data path.')
+    parser.add_argument('--epoch', type=int, default=20,
+                        help='Number of epochs.')
+    parser.add_argument('--pretrain', type=int, default=-1,
+                        help='flag for pretrain. 1: initialize from pretrain; 0: randomly initialize; -1: save to '
+                             'pretrain file')
+    parser.add_argument('--batch_size', type=int, default=4096,
+                        help='Batch size.')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='Learning rate.')
+    parser.add_argument('--optimizer', nargs='?', default='AdagradOptimizer',
+                        help='Specify an optimizer type (AdamOptimizer, AdagradOptimizer, GradientDescentOptimizer, '
+                             'MomentumOptimizer).')
+    parser.add_argument('--verbose', type=int, default=1,
+                        help='Whether to show the performance of each epoch (0 or 1)')
+    parser.add_argument('--batch_norm', type=int, default=1,
+                        help='Whether to perform batch normaization (0 or 1)')
+
+    return parser.parse_args()
+
+
+class SLR():
+    def __init__(self, features_M, pretrain_flag, save_file, epoch, batch_size, learning_rate,
+                 optimizer_type, batch_norm, verbose, random_seed=2018):
+        # bind params to class
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.save_file = save_file
+        self.pretrain_flag = pretrain_flag
+        self.features_M = features_M
+        self.epoch = epoch
+        self.random_seed = random_seed
+        self.optimizer_type = optimizer_type
+        self.batch_norm = batch_norm
+        self.verbose = verbose
+
+        # performance of each epoch
+        self.train_rmse, self.valid_rmse, self.test_rmse = [], [], []
+
+        # init all variables in a tensorflow graph
+        self._init_graph()
+
+    def _init_graph(self):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Set graph level random seed
+            tf.set_random_seed(self.random_seed)
+
+            # Input data.
+            self.train_features = tf.placeholder(tf.int32, shape=[None, None],
+                                                 name="train_features_fm")  # None * features_M
+            self.train_labels = tf.placeholder(tf.float32, shape=[None, 1], name="train_labels_fm")  # None * 1
+            self.dropout_keep = tf.placeholder(tf.float32, name="dropout_keep_fm")
+            self.train_phase = tf.placeholder(tf.bool, name="train_phase_fm")
+
+            # Variables.
+            self.weights = self._initialize_weights
+
+            # Model.
+
+            # _________out _________
+
+            # Compute the square loss.
+
+            # Optimizer.
+
+            # init
+            self.sess = self._init_session()
+            self.saver = tf.train.Saver()
+            init = tf.global_variables_initializer()
+            self.sess.run(init)
+
+            # print number of params
+            total_parameters = 0
+            for variable in self.weights.values():
+                shape = variable.get_shape()  # shape is an array of tf.Dimension
+                variable_parameters = 1
+                for dim in shape:
+                    variable_parameters *= dim.value
+                total_parameters += variable_parameters
+            if self.verbose > 0:
+                print("#params: %d" % total_parameters)
+                logging.info("#params: %d" % total_parameters)
+
+    def _init_session(self):
+        # adaptively growing video memory
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        return tf.Session(config=config)
+
+    def _initialize_weights(self):
+        all_weights = dict()
+        # 这里做的事情就是根据是否从pre-train中恢复从而进行weight的加载或初始化，需要修改
+        if self.pretrain_flag > 0:
+            weight_saver = tf.train.import_meta_graph(self.save_file + '.meta')
+            pretrain_graph = tf.get_default_graph()
+            feature_embeddings = pretrain_graph.get_tensor_by_name('feature_embeddings:0')
+            feature_bias = pretrain_graph.get_tensor_by_name('feature_bias:0')
+            bias = pretrain_graph.get_tensor_by_name('bias:0')
+
+            with self._init_session() as sess:
+                weight_saver.restore(sess, self.save_file)
+                fe, fb, b = sess.run([feature_embeddings, feature_bias, bias])
+
+            all_weights['feature_embeddings'] = tf.Variable(fe, dtype=tf.float32, name='feature_embeddings')
+            all_weights['feature_bias'] = tf.Variable(fb, dtype=tf.float32, name='feature_bias')
+            all_weights['bias'] = tf.Variable(b, dtype=tf.float32, name='bias')
+        else:
+            all_weights['feature_embeddings'] = tf.Variable(
+                tf.random_normal([self.features_M, self.hidden_factor], 0.0, 0.01),
+                name='feature_embeddings')  # features_M * K
+            all_weights['feature_bias'] = tf.Variable(
+                tf.random_uniform([self.features_M, 1], 0.0, 0.0), name='feature_bias')  # features_M * 1
+            all_weights['bias'] = tf.Variable(tf.constant(0.0), name='bias')  # 1 * 1
+        return all_weights
+
+    def batch_norm_layer(self, x, train_phase, scope_bn):
+        bn_train = batch_norm(x, decay=0.9, center=True, scale=True, updates_collections=None,
+            is_training=True, reuse=None, trainable=True, scope=scope_bn)
+        bn_inference = batch_norm(x, decay=0.9, center=True, scale=True, updates_collections=None,
+            is_training=False, reuse=True, trainable=True, scope=scope_bn)
+        z = tf.cond(train_phase, lambda: bn_train, lambda: bn_inference)
+        return z
+
+    def partial_fit(self, data):  # fit a batch
+        feed_dict = {self.train_features: data['X'], self.train_labels: data['Y'], self.dropout_keep: self.keep, self.train_phase: True}
+        loss, opt = self.sess.run((self.loss, self.optimizer), feed_dict=feed_dict)
+        return loss
+
+    def get_random_block_from_data(self, data, batch_size):  # generate a random block of training data
+        start_index = np.random.randint(0, len(data['Y']) - batch_size)
+        X , Y = [], []
+        # forward get sample
+        i = start_index
+        while len(X) < batch_size and i < len(data['X']):
+            if len(data['X'][i]) == len(data['X'][start_index]):
+                Y.append([data['Y'][i]])
+                X.append(data['X'][i])
+                i = i + 1
+            else:
+                break
+        # backward get sample
+        i = start_index
+        while len(X) < batch_size and i >= 0:
+            if len(data['X'][i]) == len(data['X'][start_index]):
+                Y.append([data['Y'][i]])
+                X.append(data['X'][i])
+                i = i - 1
+            else:
+                break
+        return {'X': X, 'Y': Y}
+
+    def shuffle_in_unison_scary(self, a, b): # shuffle two lists simutaneously
+        rng_state = np.random.get_state()
+        np.random.shuffle(a)
+        np.random.set_state(rng_state)
+        np.random.shuffle(b)
+
+    def train(self, Train_data, Validation_data, Test_data):  # fit a dataset
+        # Check Init performance
+
+        if self.verbose > 0:
+            t2 = time()
+            init_train = self.evaluate_batchly(Train_data)
+            init_valid = self.evaluate_batchly(Validation_data)
+            print("Init: \t train=%.4f, validation=%.4f [%.1f s]" % (init_train, init_valid, time() - t2))
+            logging.info("Init: \t train=%.4f, validation=%.4f [%.1f s]" % (init_train, init_valid, time() - t2))
+
+        for epoch in range(self.epoch):
+            t1 = time()
+            self.shuffle_in_unison_scary(Train_data['X'], Train_data['Y'])
+            total_batch = int(len(Train_data['Y']) / self.batch_size)
+
+            if len(Train_data['Y']) > 10000000:
+                total_batch = int(total_batch * 0.1)
+
+            for i in range(total_batch):
+                # generate a batch
+                batch_xs = self.get_random_block_from_data(Train_data, self.batch_size)
+                # Fit training
+                self.partial_fit(batch_xs)
+            t2 = time()
+
+            # output validation
+            train_result = self.evaluate_batchly(Train_data)
+            valid_result = self.evaluate_batchly(Validation_data)
+
+            self.train_rmse.append(train_result)
+            self.valid_rmse.append(valid_result)
+
+            if self.verbose > 0 and epoch % self.verbose == 0:
+                print("Epoch %d [%.1f s]\ttrain=%.4f, validation=%.4f [%.1f s]"
+                      % (epoch + 1, t2 - t1, train_result, valid_result, time() - t2))
+                logging.info("Epoch %d [%.1f s]\ttrain=%.4f, validation=%.4f [%.1f s]"
+                             % (epoch + 1, t2 - t1, train_result, valid_result, time() - t2))
+            if self.eva_termination(self.valid_rmse):
+                break
+
+        if self.pretrain_flag < 0:
+            print("Save model to file as pretrain.")
+            logging.info("Save model to file as pretrain.")
+            self.saver.save(self.sess, self.save_file)
+
+    def eva_termination(self, valid):
+        if len(valid) > 5:
+            if valid[-1] > valid[-2] and valid[-2] > valid[-3] and valid[-3] > valid[-4] and valid[-4] > valid[-5]:
+                return True
+        return False
+
+    def evaluate(self, data):  # evaluate the results for an input set
+        num_example = len(data['Y'])
+        feed_dict = {self.train_features: data['X'], self.train_labels: [[y] for y in data['Y']],
+                     self.dropout_keep: 1.0, self.train_phase: False}
+        predictions = self.sess.run((self.out), feed_dict=feed_dict)
+        y_pred = np.reshape(predictions, (num_example,))
+        y_true = np.reshape(data['Y'], (num_example,))
+
+        predictions_bounded = np.maximum(y_pred, np.ones(num_example) * min(y_true))  # bound the lower values
+        predictions_bounded = np.minimum(predictions_bounded,
+                                         np.ones(num_example) * max(y_true))  # bound the higher values
+        RMSE = math.sqrt(mean_squared_error(y_true, predictions_bounded))
+        return RMSE
+
+    def evaluate_batchly(self, data):  # evaluate the results for a batch of an input set
+        num_example = len(data['Y'])
+
+        predictions = []
+        for i in range(math.ceil(num_example / self.batch_size)):
+            end = (i+1) * self.batch_size if (i+1) * self.batch_size < num_example else num_example
+            feed_dict = {self.train_features: data['X'][i * self.batch_size : end], self.train_labels: [[y] for y in data['Y'][i * self.batch_size : end]],
+                     self.dropout_keep: 1.0, self.train_phase: False}
+            predictions_batch = self.sess.run((self.out), feed_dict=feed_dict)
+            predictions.extend(predictions_batch.tolist())
+
+        y_pred = np.reshape(predictions, (num_example,))
+        y_true = np.reshape(data['Y'], (num_example,))
+
+        predictions_bounded = np.maximum(y_pred, np.ones(num_example) * min(y_true))  # bound the lower values
+        predictions_bounded = np.minimum(predictions_bounded,
+                                         np.ones(num_example) * max(y_true))  # bound the higher values
+        RMSE = math.sqrt(mean_squared_error(y_true, predictions_bounded))
+
+        return RMSE
+
+def make_save_file(args):
+    pretrain_path = '../pretrain/fm_%s_%d' % (args.dataset, args.hidden_factor)
+    if not os.path.exists(pretrain_path):
+        os.makedirs(pretrain_path)
+    save_file = pretrain_path + '/%s_%d' % (args.dataset, args.hidden_factor)
+    return save_file
+
+def make_log_file(args):
+    pretrain_path = '../pretrain/fm_%s_%d.log' % (args.dataset, args.hidden_factor)
+    if not os.path.exists(pretrain_path):
+        f = open(pretrain_path, 'w')
+        f.close()
+    return pretrain_path
+
+
+def train(args):
+    # Data loading
+    data = DATA.LoadData(args.path, args.dataset)
+    if args.verbose > 0:
+        #下面的需要改
+        print(
+            "FM: dataset=%s, factors=%d, #epoch=%d, batch=%d, lr=%.4f, lambda=%.1e, keep=%.2f, optimizer=%s, batch_norm=%d"
+            % (args.dataset, args.hidden_factor, args.epoch, args.batch_size, args.lr, args.lamda, args.keep,
+               args.optimizer, args.batch_norm))
+        logging.info(
+            "FM: dataset=%s, factors=%d, #epoch=%d, batch=%d, lr=%.4f, lambda=%.1e, keep=%.2f, optimizer=%s, batch_norm=%d"
+            % (args.dataset, args.hidden_factor, args.epoch, args.batch_size, args.lr, args.lamda, args.keep,
+               args.optimizer, args.batch_norm))
+
+    # Training
+    t1 = time()
+    model = SLR()
+    model.train(data.Train_data, data.Validation_data, data.Test_data)
+
+    # Find the best validation result across iterations
+    best_valid_score = 0
+    best_valid_score = min(model.valid_rmse)
+    best_epoch = model.valid_rmse.index(best_valid_score)
+    print("Best Iter(validation)= %d\t train = %.4f, valid = %.4f [%.1f s]"
+          % (best_epoch + 1, model.train_rmse[best_epoch], model.valid_rmse[best_epoch], time() - t1))
+    logging.info("Best Iter(validation)= %d\t train = %.4f, valid = %.4f [%.1f s]"
+                 % (best_epoch + 1, model.train_rmse[best_epoch], model.valid_rmse[best_epoch], time() - t1))
+
+
+def evaluate(args):
+    # load test data
+    data = DATA.LoadData(args.path, args.dataset).Test_data
+    save_file = make_save_file(args)
+
+    # load the graph
+    weight_saver = tf.train.import_meta_graph(save_file + '.meta')
+    pretrain_graph = tf.get_default_graph()
+
+    # load tensors
+    feature_embeddings = pretrain_graph.get_tensor_by_name('feature_embeddings:0')
+    nonzero_embeddings = pretrain_graph.get_tensor_by_name('nonzero_embeddings:0')
+    feature_bias = pretrain_graph.get_tensor_by_name('feature_bias:0')
+    bias = pretrain_graph.get_tensor_by_name('bias:0')
+    fm = pretrain_graph.get_tensor_by_name('fm:0')
+    fm_out = pretrain_graph.get_tensor_by_name('fm_out:0')
+    out = pretrain_graph.get_tensor_by_name('out:0')
+    train_features = pretrain_graph.get_tensor_by_name('train_features_fm:0')
+    train_labels = pretrain_graph.get_tensor_by_name('train_labels_fm:0')
+    dropout_keep = pretrain_graph.get_tensor_by_name('dropout_keep_fm:0')
+    train_phase = pretrain_graph.get_tensor_by_name('train_phase_fm:0')
+
+    # restore session
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    weight_saver.restore(sess, save_file)
+
+    # start evaluation
+    num_example = len(data['Y'])
+    feed_dict = {train_features: data['X'], train_labels: [[y] for y in data['Y']], dropout_keep: 1.0,
+                 train_phase: False}
+    ne, fe = sess.run((nonzero_embeddings, feature_embeddings), feed_dict=feed_dict)
+    _fm, _fm_out, predictions = sess.run((fm, fm_out, out), feed_dict=feed_dict)
+
+    # calculate rmse
+    y_pred = np.reshape(predictions, (num_example,))
+    y_true = np.reshape(data['Y'], (num_example,))
+
+    predictions_bounded = np.maximum(y_pred, np.ones(num_example) * min(y_true))  # bound the lower values
+    predictions_bounded = np.minimum(predictions_bounded, np.ones(num_example) * max(y_true))  # bound the higher values
+    RMSE = math.sqrt(mean_squared_error(y_true, predictions_bounded))
+
+    print("Test RMSE: %.4f" % (RMSE))
+    logging.info("Test RMSE: %.4f" % (RMSE))
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    log_file = make_log_file(args)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG, filename=log_file)
+
+    # initialize the optimal parameters
+    # if args.mla:
+    #     args.lr = 0.05
+    #     args.keep = 0.7
+    #     args.batch_norm = 0
+    # else:
+    #     args.lr = 0.01
+    #     args.keep = 0.7
+    #     args.batch_norm = 1
+
+    if args.process == 'train':
+        train(args)
+    elif args.process == 'evaluate':
+        evaluate(args)
